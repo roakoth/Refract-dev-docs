afcd545ae4dd04625cb7c70750eaa1c9581e10ef
Rose Akoth <roseakoth@Roses-MacBook-Air.local>
2025-09-16 21:38:42 +0200

    added styling to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner, styles.headerWrapper)}>
       <div className="container">
         <Heading as="h1" className="text--center padding-horiz--md">
            {siteConfig.title}
